227005bd36305fb0e75d5632dcdc2f6815ee3b01
nkosi-tauro <nkosilati23@yahoo.com>
2022-04-28 13:25:01 +0200

    Added base functions

---
+++ phonebook.py
@@ -0,0 +1,28 @@
+# Welcome to the Rubiks Phone Book.
+import time
+
+#Dictionary of contacts
+# This will be used to store the contacts.
+phonebook = {}
+program_open = True
+
+def add_contact(key, value) :
+  """Adds a new contact to the phonebook.""" 
+  return phonebook
+
+def search_contact(key) :
+  """Searches for a contact in the phonebook."""
+  return phonebook
+
+def delete_contact(key) :
+  """Deletes a contact from the phonebook."""
+  return print(phonebook)
+
+def sort_contact() :
+  """Sorts the contacts in the phonebook."""
+  return phonebook
+
+
+def display_contacts() :
+  """Displays all contacts in the phonebook."""
+  return phonebook
