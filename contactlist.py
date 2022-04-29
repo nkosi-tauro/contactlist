9aa18fea144bca8f303411333f64d3bcf605d26a
nkosi-tauro <nkosilati23@yahoo.com>
2022-04-29 10:59:38 +0200

    Added Edit functionality

--- contactlist.py
+++ contactlist.py
@@ -22,6 +22,19 @@ def delete_contact(key) :
   contactlist.pop(key)
   return print(f"{key} has been deleted from the Contact List.\n")
 
+def edit_contact(key) :
+  """Edits a contact in the contactlist."""
+  isNumber = False
+  while not isNumber:
+    try :
+      contact_number_update = int(input('Enter the updated contact number: '))
+      isNumber = True
+    except ValueError:
+      print('The number must be a number.\n')
+  contactlist.update({key : contact_number_update})
+  print(f"The Contact Name: '{key}' has been edited with the new Contact Number: '{contact_number_update}'.\n")
+  return contactlist
+
 def sort_contacts() :
   """Sort the contacts in the contactlist by alpabetical order"""
   print("Rubicks Phone Book\n")
@@ -51,6 +64,7 @@ def display_contacts() :
         contact_number = value
         print ("{:<10} {:<10}".format(key, contact_number))
 
+# Validation Methods for user input
 def contact_validation() :
   """Validate the contact name and number before they are entered into the contactlist."""
   # check if contact name is greater or equal to 5 characters
@@ -74,14 +88,17 @@ def contact_validation() :
 
   return contact_name, contact_number
 
+# Validation to check if key is in contactlist
 def contact_exists(choice) :
   """Checks if selected key/contact name to be exists in the contactlist."""
   contact_name = input('Enter the name of the contact: ')
   # check if contact name is in the contactlist and then either delete or search for the contact based on user choice
-  if contact_name in contactlist and choice == 3:
-    delete_contact(contact_name)
-  elif contact_name in contactlist and choice == 2:
+  if contact_name in contactlist and choice == 2:
     search_contact(contact_name)
+  elif contact_name in contactlist and choice == 3:
+    delete_contact(contact_name)
+  elif contact_name in contactlist and choice == 4:
+    edit_contact(contact_name)
   else:
     print(f'The Contact Name: "{contact_name}" does not exist in the contactlist.\n')
   
@@ -93,9 +110,9 @@ while program_open:
     print('Welcome to the Rubiks Contact List.\n')
     print('What would you like to do?\n')
     print('1. Add a new contact')
-    print('2. Search a contact')
+    print('2. Search for a contact')
     print('3. Delete a contact')
-    print('4. Sort contacts')
+    print('4. Edit a contact')
     print('5. Display all contacts')
     print('6. Quit the program')
     print('\n')
@@ -124,7 +141,9 @@ while program_open:
       time.sleep(2)
 
     elif choice == 4 :
-      sort_contacts()
+      print('Edit a contact from the contactlist.\n')
+      # Validate if the contact name to be edited exists in the contactlist.
+      contact_exists(choice)
       print("You will be returned to the main menu in 2 seconds.\n")
       time.sleep(2)
 
